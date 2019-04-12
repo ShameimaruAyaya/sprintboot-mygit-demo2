d4592e45283f0d47053d3e767ccaeff1c4e2f927
Shameimaru Aya <1553351623@qq.com>
2019-04-12 14:07:50 +0800

    v1.0

--- src/main/java/com/baizhi/SpringbootMygitDemo2Application.java
+++ src/main/java/com/baizhi/SpringbootMygitDemo2Application.java
@@ -8,6 +8,8 @@ public class SpringbootMygitDemo2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMygitDemo2Application.class, args);
-    }
 
+
+
+    }
 }
