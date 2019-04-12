8622ae196d874252387b33ceb2cc2c48a1e2a9e2
Shameimaru Aya <1553351623@qq.com>
2019-04-12 11:10:41 +0800

    Initial commit

---
+++ src/main/java/com/baizhi/SpringbootMygitDemo2Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMygitDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMygitDemo2Application.class, args);
+    }
+
+}
